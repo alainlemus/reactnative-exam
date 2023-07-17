ce7434e3506aa3ef8e68f46dd41f856924e3cd54
alainink <alain@inkwonders.com>
2023-07-17 01:45:59 -0600

    limpiando datos

--- exam/App.js
+++ exam/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
@@ -46,12 +43,3 @@ export default function App() {
 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
